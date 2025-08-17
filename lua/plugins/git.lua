bb2fc9b3cdafc003b5be86281e06e6b5fae83f31
Chris Guarino <17974188+Fury1@users.noreply.github.com>
2025-08-17 13:04:24 -0400

    feat: Add gitsigns hunk keymaps & load on start

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,11 +1,21 @@
 return {
 	{
 		"lewis6991/gitsigns.nvim",
-        lazy = false,
+		lazy = false,
 		opts = {},
 		keys = {
 			{ "<leader>gp", "<cmd>Gitsigns preview_hunk<CR>" },
-			{ "<leader>gh", "<cmd>Gitsigns stage_hunk<CR>" },
+			{ "<leader>ghs", "<cmd>Gitsigns stage_hunk<CR>", mode = { "n" } },
+			{
+				"<leader>ghs",
+				function()
+					require("gitsigns").stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
+				end,
+				mode = { "v" },
+			},
+			{ "<leader>ghu", "<cmd>Gitsigns undo_stage_hunk<CR>" },
+			{ "<leader>ghn", "<cmd>Gitsigns nav_hunk next<CR>" },
+			{ "<leader>ghp", "<cmd>Gitsigns nav_hunk prev<CR>" },
 		},
 	},
 	{
