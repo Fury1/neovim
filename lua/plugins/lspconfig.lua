394b00cda1e98be5a58ca08f76de6dfefc22bab6
Chris Guarino <17974188+Fury1@users.noreply.github.com>
2025-08-18 08:53:23 -0400

    feat(ui): Add LSP gutter icons to replace "H,W,E,.."

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -7,6 +7,14 @@ return {
 
 		-- LSP message appearance
 		vim.diagnostic.config({
+			signs = {
+				text = {
+					[vim.diagnostic.severity.ERROR] = "",
+					[vim.diagnostic.severity.WARN] = "",
+					[vim.diagnostic.severity.INFO] = "",
+					[vim.diagnostic.severity.HINT] = "󰌵",
+				},
+			},
 			-- virtual_lines = { current_line = true },
 			virtual_text = {
 				source = true,
