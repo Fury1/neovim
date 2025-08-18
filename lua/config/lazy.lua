3edbb042cddad1314d5a9a4af13deb6944146d00
Chris Guarino <17974188+Fury1@users.noreply.github.com>
2025-08-18 08:47:17 -0400

    style: Reformat with stylua

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -1,13 +1,13 @@
 -- Bootstrap lazy.nvim
-local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-	local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
-	local out = vim.fn.system({ 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath })
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
 	if vim.v.shell_error ~= 0 then
 		vim.api.nvim_echo({
-			{ 'Failed to clone lazy.nvim:\n', 'ErrorMsg' },
-			{ out, 'WarningMsg' },
-			{ '\nPress any key to exit...' },
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+			{ "\nPress any key to exit..." },
 		}, true, {})
 		vim.fn.getchar()
 		os.exit(1)
@@ -25,10 +25,14 @@ vim.opt.rtp:prepend(lazypath)
 -- vim.g.maplocalleader = '\\'
 
 -- Setup lazy.nvim
-require('lazy').setup({
+require("lazy").setup({
 	spec = {
 		-- Import your plugins.
-		{ import = 'plugins' },
+		{ import = "plugins" },
+	},
+	ui = {
+		-- a number <1 is a percentage., >1 is a fixed size
+		size = { width = 110, height = 0.8 },
 	},
 	-- Automatically check for plugin updates.
 	checker = { enabled = true },
