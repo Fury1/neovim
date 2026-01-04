c537636cbb2044b035f69fde66585d440010f124
Chris Guarino <17974188+Fury1@users.noreply.github.com>
2026-01-04 15:53:49 -0500

    refactor: Reorder Flash keybind

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -15,10 +15,10 @@ return {
 	keys = {
 		{
 			"gw",
-			mode = { "n", "x", "o" },
 			function()
 				require("flash").jump()
 			end,
+			mode = { "n", "x", "o" },
 		},
 		-- { "gW", mode = { "n", "x", "o" }, function() require("flash").treesitter() end },
 		-- { "r", mode = "o", function() require("flash").remote() end },
