57863b2f97121fbe0ecb17e634885056175dd237
Chris Guarino <17974188+Fury1@users.noreply.github.com>
2026-01-05 14:01:40 -0500

    refactor: Lazy load inline markview

--- lua/plugins/markview.lua
+++ lua/plugins/markview.lua
@@ -1,7 +1,15 @@
 return {
 	"OXY2DEV/markview.nvim",
-	lazy = false,
+	lazy = true,
 
 	-- Completion for `blink.cmp`
 	dependencies = { "saghen/blink.cmp" },
+	opts = {
+		preview = {
+			enable = false,
+		},
+	},
+	keys = {
+		{ "<leader>p", "<cmd>Markview Toggle<CR>", desc = "Toggle Markview inline preview" },
+	},
 }
